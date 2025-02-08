9bbad8fc5e0236ae207bac5c6b71ed90a57cfb56
blexc <alex.tchap99@gmail.com>
2025-02-07 19:14:53 -0800

    wip local fine tuning

--- scripts/fine-tune.py
+++ scripts/fine-tune.py
@@ -1,15 +1,151 @@
 from unsloth import FastLanguageModel
+from datasets import load_dataset
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from unsloth import is_bfloat16_supported
 import torch
-max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
-dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
 
-model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "unsloth/DeepSeek-R1-Distill-Llama-8B-unsloth-bnb-4bit",
-    max_seq_length = max_seq_length,
-    dtype = dtype,
-    load_in_4bit = load_in_4bit,
-)
+import os
+from helper import repo_dir
+
+models_to_try = [
+    "unsloth/Meta-Llama-3.1-8B-bnb-4bit",
+    "unsloth/Meta-Llama-3.1-8B",
+]
+
+model_name = os.path.join(repo_dir, "models", "Meta-Llama-3.1-8B")
+max_seq_length = 2048
+dtype = None
+load_in_4bit = True
+bamchan_prompt = """Below is an input message that is part of a longer conversation. Write a response that appropriately completes the request.
+
+### Input:
+{}
+
+### Response:
+{}"""
+
+
+def load_model():
+    # Load model
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name = model_name,
+        max_seq_length = max_seq_length,
+        dtype = dtype,
+        load_in_4bit = load_in_4bit,
+    )
+
+    # Add LoRA adapters to save memory usage
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r = 16,
+        target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                        "gate_proj", "up_proj", "down_proj",],
+        lora_alpha = 16,
+        lora_dropout = 0,
+        bias = "none",
+        use_gradient_checkpointing = "unsloth",
+        random_state = 3407,
+        use_rslora = False,
+        loftq_config = None,
+    )
+
+    return model, tokenizer
+
+
+def prep_dataset(tokenizer):
+    EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
+    def formatting_prompts_func(examples):
+        inputs       = examples["input"]
+        outputs      = examples["output"]
+        texts = []
+        for input, output in zip(inputs, outputs):
+            # Must add EOS_TOKEN, otherwise your generation will go on forever!
+            text = bamchan_prompt.format(input, output) + EOS_TOKEN
+            texts.append(text)
+        return { "text" : texts, }
+    pass
+
+    from datasets import load_dataset
+    dataset = load_dataset("blexchapman/bam-chan", split = "train")
+    dataset = dataset.map(formatting_prompts_func, batched = True,)
+    return dataset
+
+
+def print_memory_stats():
+    gpu_stats = torch.cuda.get_device_properties(0)
+    start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+    max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+    print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+    print(f"{start_gpu_memory} GB of memory reserved.")
+    return start_gpu_memory, max_memory
+
+
+def print_final_memory_and_time_stats(start_gpu_memory, max_memory):
+    used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+    used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
+    used_percentage = round(used_memory / max_memory * 100, 3)
+    lora_percentage = round(used_memory_for_lora / max_memory * 100, 3)
+    print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
+    print(
+        f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training."
+    )
+    print(f"Peak reserved memory = {used_memory} GB.")
+    print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
+    print(f"Peak reserved memory % of max memory = {used_percentage} %.")
+    print(f"Peak reserved memory for training % of max memory = {lora_percentage} %.")
+
 
 if __name__ == "__main__":
-    print("fine tuning")
+
+    # Load model
+    model, tokenizer = load_model()
+
+    # Prepare dataset
+    dataset = prep_dataset(tokenizer)
+
+    # Train model
+    trainer = SFTTrainer(
+        model = model,
+        tokenizer = tokenizer,
+        train_dataset = dataset,
+        dataset_text_field = "text",
+        max_seq_length = max_seq_length,
+        dataset_num_proc = 2,
+        packing = False, # Can make training 5x faster for short sequences.
+        args = TrainingArguments(
+            per_device_train_batch_size = 2,
+            gradient_accumulation_steps = 4,
+            warmup_steps = 5,
+            # num_train_epochs = 1, # Uncomment this for 1 full training run.
+            max_steps = 60, # 60 for fast run, None for full training run.
+            learning_rate = 2e-4,
+            fp16 = not is_bfloat16_supported(),
+            bf16 = is_bfloat16_supported(),
+            logging_steps = 1,
+            optim = "adamw_8bit",
+            weight_decay = 0.01,
+            lr_scheduler_type = "linear",
+            seed = 3407,
+            output_dir = "outputs",
+            report_to = "none", # Use this for WandB etc
+        ),
+    )
+
+    start_gpu_memory, max_memory = print_memory_stats()
+
+    trainer_stats = trainer.train()
+
+    print_final_memory_and_time_stats(start_gpu_memory, max_memory)
+
+    # Test model
+    FastLanguageModel.for_inference(model)
+    inputs = tokenizer(
+    [
+        bamchan_prompt.format(
+            "hey there!", # input
+            "", # output - leave this blank for generation!
+        )
+    ], return_tensors = "pt").to("cuda")
+    outputs = model.generate(**inputs, max_new_tokens = 64, use_cache = True)
+    tokenizer.batch_decode(outputs)
